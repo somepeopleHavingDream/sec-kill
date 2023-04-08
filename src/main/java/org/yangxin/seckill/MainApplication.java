1d5b77ffee60cd05aa848a32337899d593056195
yangxin <1393003255@qq.com>
2023-04-08 22:56:26 +0800

    feat: 命令行压测上

--- src/main/java/org/yangxin/seckill/MainApplication.java
+++ src/main/java/org/yangxin/seckill/MainApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecKillApplication {
+public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecKillApplication.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 }
